2acbb7af5ff6fa73dde05304d682f7322110bd75
LeoLanzieri <leolanzieri1@gmail.com>
2019-05-07 16:35:56 -0300

    Se corrigen los archivos para la entrega, se crea la carpeta Reentrega, se agrega txt con link al archivo ejecutable v1

--- makefile.bat
+++ makefile.bat
@@ -2,13 +2,13 @@ flex lexico.l
 pause
 bison -dyv sintactico.y
 pause
-gcc.exe lex.yy.c y.tab.c -o ejecutable.exe
+gcc.exe lex.yy.c y.tab.c -o Primera.exe
 pause
 pause
-primera.exe prueba.txt
+Primera.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
-del ejecutable.exe
+del primera.exe
 pause
